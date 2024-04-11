42dc2db9702c3a8397466fb89dda22cd1d6882d9
lax1dude <cgiacun@gmail.com>
2024-04-10 20:19:33 -0700

    fix filename

--- java/net/lax1dude/unsafememcpy/UnsafeUtils.java
+++ java/net/lax1dude/unsafememcpy/UnsafeUtils.java
@@ -40,7 +40,11 @@ public class UnsafeUtils {
 	public static void loadNatives() {
 		if(!hasLoadedNatives) {
 			hasLoadedNatives = true;
-			System.loadLibrary("UnsafeMemcpy");
+			if(System.getProperty("os.name").toLowerCase().contains("windows")) {
+				System.loadLibrary("UnsafeMemcpy");
+			}else {
+				System.loadLibrary("unsafememcpy");
+			}
 		}
 	}
 
